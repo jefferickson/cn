2d493a5a1a69ba88328dfc5391544140be979cb1
Jeff Erickson <jeff@erick.so>
2019-01-21 19:23:58 -0500

    add option to return data

--- main.go
+++ main.go
@@ -4,63 +4,106 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
-func readRow(fileName string, row int) (string, error) {
+var args []string
+var printData = false
+
+type cache []string
+
+func init() {
+	const usageText = "USAGE: cn [-d] LABEL FILE"
+
+	args = os.Args[1:]
+	if len(args) < 2 || len(args) > 3 {
+		fmt.Println(usageText)
+		os.Exit(1)
+	} else if len(args) == 3 && args[0] != "-d" {
+		fmt.Println(usageText)
+		os.Exit(1)
+	} else if len(args) == 3 {
+		printData = true
+		args = args[1:]
+	}
+}
+
+func main() {
+	var fileCache cache
+
+	err := fileCache.readData(args[1], !printData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else if len(fileCache) == 0 {
+		os.Exit(1)
+	}
+
+	value, ok := findNumber(fileCache[0], args[0], ",")
+	if !ok {
+		os.Exit(1)
+	}
+
+	if printData {
+		for _, row := range fileCache[1:] {
+			if cell := extractCol(row, value, ","); cell != "" {
+				fmt.Println(cell)
+			}
+		}
+	} else {
+		fmt.Println(value)
+	}
+}
+
+func (c *cache) readData(fileName string, firstRowOnly bool) error {
 	var scanner *bufio.Scanner
+
+	if len(*c) != 0 {
+		return nil
+	}
 
 	if fileName == "-" {
 		scanner = bufio.NewScanner(os.Stdin)
 	} else {
 		file, err := os.Open(fileName)
 		if err != nil {
-			return "", err
+			return err
 		}
 		defer file.Close()
 
 		scanner = bufio.NewScanner(file)
 	}
 
-	rowRead := 0
 	for scanner.Scan() {
-		if rowRead == row {
-			return scanner.Text(), scanner.Err()
+		*c = append(*c, scanner.Text())
+		if firstRowOnly {
+			return scanner.Err()
 		}
-		rowRead++
 	}
 
-	return "", nil
+	return scanner.Err()
 }
 
-func findNumber(rowData string, label string, delim string) string {
+func findNumber(rowData string, label string, delim string) (int, bool) {
 	s := strings.Split(rowData, delim)
 
 	for n, header := range s {
 		if header == label {
-			return strconv.Itoa(n + 1)
+			return n + 1, true
+		}
+	}
+
+	return 0, false
+}
+
+func extractCol(rowData string, col int, delim string) string {
+	s := strings.Split(rowData, delim)
+
+	for n, colText := range s {
+		if n+1 == col {
+			return colText
 		}
 	}
 
 	return ""
-}
-
-func main() {
-	usageText := "USAGE: cn LABEL FILE"
-
-	args := os.Args[1:]
-	if len(args) < 2 {
-		fmt.Println(usageText)
-		os.Exit(1)
-	}
-
-	data, err := readRow(args[1], 0)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	value := findNumber(data, args[0], ",")
-	fmt.Println(value)
 }
